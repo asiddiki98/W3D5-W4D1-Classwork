27d9957828d7f27a1e3e22ed3d1b44b362c6bbc6
asiddiki98 <azimsardines@gmail.com>
2020-11-16 15:16:37 -0500

    Almost finished with Part 1

--- skeleton/lib/knights_travails.rb
+++ skeleton/lib/knights_travails.rb
@@ -20,23 +20,29 @@ class KnightPathFinder
     def initialize(pos)
         @pos = pos
         @root_node = PolyTreeNode.new(pos)
-        # self.build_move_tree
-        @considered_positions = [pos]
+        @considered_positions = [@root_node]
+        self.build_move_tree
     end
 
-    # def build_move_tree # remove first move position
-    #     @root_node
-    #     current_pos = []
-    #     new_move_positions(current_pos).each do |next_pos|
-
-    #     end
-        
-    # end
+    def build_move_tree # remove first move position
+       queue = [@root_node]
+        until queue.empty?
+            el = queue.shift
+            new_move_positions(el.value).each do |move|
+                child = PolyTreeNode.new(move)
+                child.parent = el
+                queue << child
+            end      
+        end 
+    end
 
     def new_move_positions(pos)
         new_move_pos = []
+        # debugger
         valid_move = KnightPathFinder.valid_moves(pos)
+        # debugger
         valid_move.each do |move|
+            # debugger
             if !@considered_positions.include?(move)
                 new_move_pos << move
                 @considered_positions << move  
@@ -53,7 +59,7 @@ class KnightPathFinder
                  new_x = x + move[0]
                 if (y + move[1]) >= 0 && (y + move[1]) <= 7
                     new_y = y + move[1]
-                    valid_moves << [new_x, new_y]
+                    valid_moves << [new_x, new_y] 
                 end
             end
         end
@@ -61,7 +67,7 @@ class KnightPathFinder
     end
 end
 
-first_move = KnightPathFinder.new([0,0])
+p first_move = KnightPathFinder.new([0,0])
 # p KnightPathFinder.valid_moves([0, 0])
 
-p first_move.new_move_positions([0, 0])
+# p first_move.new_move_positions([0, 0])
